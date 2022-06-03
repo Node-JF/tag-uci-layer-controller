0e68334ac6e4535a7eba1d0a2b4369c8478a40a1
Jason Foord <jf@tag.com.au>
2022-06-03 10:22:19 +0930

    update structure

--- src/info.lua
+++ src/info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   ['Name'] = "TAG~UCI Layer Controller",
   ['Version'] = "1.0",
-  ['BuildVersion'] = "1.0.0.2",
+  ['BuildVersion'] = "1.0.0.3",
   ['Id'] = "b37b2499-7d80-4fb4-8cff-169fc334669a",
   ['Description'] = "",
   ['ShowDebug'] = false
